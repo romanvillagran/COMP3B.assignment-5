fa2cb790d30eb7a9d7d0716d7269e3f54148ee95
romanvillagran <romanvillagran386@gmail.com>
2025-04-24 12:07:19 -0700

    added 3 books, there titles, authors , and release date

--- COMP3B.assignment 5/Data/LibraryData.cs
+++ COMP3B.assignment 5/Data/LibraryData.cs
@@ -1,6 +1,13 @@
-﻿namespace COMP3B.assignment_5.Data
+﻿using COMP3B.assignment_5.Models;
+namespace COMP3B.assignment_5.Data
 {
     public class LibraryData
     {
+        public static List<Book> Books = new List<Book>
+        {
+            new Book { Id = 1, Title = "The Fellowship of the Ring", Author = "John Ronald Reuel Tolkien", YearPublished = 1954, IsAvailable = true },
+            new Book { Id = 2, Title = "The Hobbit", Author = "John Ronald Rreuel Tolkien", YearPublished = 1937, IsAvailable = false },
+            new Book { Id = 3, Title = "Berserk", Author = "Kentaro Miura", YearPublished = 1989, IsAvailable = true }
+        };
     }
 }
