ec2740ac7a455219d135809a1de5f1027d185a73
romanvillagran <romanvillagran386@gmail.com>
2025-04-24 13:12:25 -0700

    added he action result

--- COMP3B.assignment 5/Controllers/LibraryController.cs
+++ COMP3B.assignment 5/Controllers/LibraryController.cs
@@ -1,4 +1,5 @@
-﻿using COMP3B.assignment_5.Models;
+﻿using COMP3B.assignment_5.Data;
+using COMP3B.assignment_5.Models;
 using Microsoft.AspNetCore.Mvc;
 namespace COMP3B.assignment_5.Controllers
 { 
@@ -7,7 +8,27 @@ namespace COMP3B.assignment_5.Controllers
 
     public class LibraryController : Controller
     {
-       
         
+        [HttpGet]
+        public ActionResult<List<Book>> GetAllBooks()
+        {
+            
+            return Ok(LibraryData.Books);
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Book> GetBook(int id)
+        {
+            var book = LibraryData.Books.Find(b => b.Id == id);
+
+            if (book == null)
+            {
+                return NotFound();
+            }
+
+            
+            return Ok(book);
+        }
+
     }
 }
